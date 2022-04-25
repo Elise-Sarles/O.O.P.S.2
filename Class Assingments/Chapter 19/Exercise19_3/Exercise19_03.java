8d036ef988ecfbaac34a07364be27ee223f315fa
Elise-Sarles <87583973+Elise-Sarles@users.noreply.github.com>
2022-04-25 10:17:26 -0600

    Add files via upload

---
+++ Class Assingments/Chapter 19/Exercise19_3/Exercise19_03.java
@@ -0,0 +1,32 @@
+/*
+Author:Elise
+Date:Apr 25
+
+Description: Remover duplicates from an array list
+ */
+import java.util.ArrayList;
+
+public class Exercise19_03 {
+  public static void main(String[] args) {
+    ArrayList<Integer> list = new ArrayList<Integer>();
+    list.add(14);
+    list.add(24);
+    list.add(14);
+    list.add(42);
+    list.add(25);
+    
+    ArrayList<Integer> newList = removeDuplicates(list);
+    
+    System.out.print(newList);
+  }
+  public static ArrayList removeDuplicates(ArrayList<Integer> list){
+    for(int i = 0; i < list.size() - 1; i++){
+      for (int j = i + 1; j < list.size(); j++){
+        if (list.get(i).compareTo(list.get(j)) == 0) {
+          list.remove(j);
+        }
+      }
+    }
+     return list;
+  }
+}
